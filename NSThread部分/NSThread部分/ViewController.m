4ad77b25ed061e9f9e32464bf54e232b109793e2
康佳兴 <168568123@qq.com>
2017-08-04 18:20:31 +0800

    线程间通信

--- NSThread部分/NSThread部分/ViewController.m
+++ NSThread部分/NSThread部分/ViewController.m
@@ -12,6 +12,7 @@
 @interface ViewController ()
 
 @property (nonatomic,assign)NSInteger ticketCount;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -21,11 +22,69 @@
     
     [super viewDidLoad];
     
-    self.ticketCount = 100;
+    //self.ticketCount = 100;
    
-    [self testSaleTicket];
+    //[self testSaleTicket];
 }
 
+- (void)testSocket
+{
+    
+}
+
+
+
+/**
+ 线程间通信
+ */
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //创建子线程(3种方法)
+    [NSThread detachNewThreadSelector:@selector(download) toTarget:self withObject:nil];
+}
+
+#pragma mark -----------------------
+#pragma mark Methods
+
+-(void)download
+{
+    NSLog(@"download----%@",[NSThread currentThread]);
+    
+    //01 确定URL地址
+    NSURL *url = [NSURL URLWithString:@"http://image.tianjimedia.com/uploadImages/2015/083/30/VVJ04M7P71W2.jpg"];
+    
+    //02 把图片的二进制数据下载到本地
+    NSData *imageData = [NSData dataWithContentsOfURL:url];
+    
+    //03 把图片的二进制格式转换为UIimage
+    UIImage *image = [UIImage imageWithData:imageData];
+    
+    //报错:把和UI相关的操作放在后台线程中处理
+    
+    //04 回到主线程显示图片
+    //子线程切换回主线程
+    /* 参数说明
+     *
+     * 第一个参数:方法选择器  回到主线程要做什么(方法)
+     * 第二个参数:调用函数需要传递的参数
+     * 第三个参数:是否等待该方法执行完毕才继续往下执行 YES:等待
+     */
+    //第一种方法
+    //[self performSelectorOnMainThread:@selector(showImage:) withObject:image waitUntilDone:YES];
+    
+    //第二种方法
+    [self performSelector:@selector(showImage:) onThread:[NSThread mainThread] withObject:image waitUntilDone:YES];
+    
+    //简便方法
+    //[self.imageView performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:YES];
+    
+    NSLog(@"_____end______");
+}
+
+-(void)showImage:(UIImage *)image
+{
+    self.imageView.image = image;
+}
 
 /**
  线程安全
@@ -63,6 +122,7 @@
 
 
 
+
 /**
  线程优先级
  * 可以看成执行次数threadA > threadB > threadC
@@ -73,6 +133,7 @@
     threadA.name = @"threadA";
     threadA.threadPriority = 1.0;
     [threadA start];
+    
     
     
     NSThread *threadB = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"threadB"];
@@ -93,6 +154,18 @@
     for (NSInteger i = 0; i < 5; i++) {
         NSLog(@"%zd -- %@",i , [NSThread currentThread].name);
     }
+    
+    //绝对时间
+    CFTimeInterval start = CFAbsoluteTimeGetCurrent();
+    
+    //耗时操作
+    
+    CFTimeInterval end = CFAbsoluteTimeGetCurrent();
+    
+    NSLog(@"%f",end - start);
+    
+
+    
 }
 
 
