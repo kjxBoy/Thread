8b5609faf12eb9e9928eb0172e9121ecd7105392
康佳兴 <168568123@qq.com>
2017-08-11 10:30:18 +0800

    Runloop 部分

---
+++ runloop部分/runloop部分/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runloop部分
+//
+//  Created by apple on 2017/8/11.
+//  Copyright © 2017年 Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runloop部分/runloop部分/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runloop部分
+//
+//  Created by apple on 2017/8/11.
+//  Copyright © 2017年 Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
